0baf834f08122ee669f86ac4d255aae9a402e58e
Chloé Briquet <ch.briquet@outlook.com>
2019-10-24 14:15:08 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vehicule Rental Microservices Project
+
+## Customers & Authentication
---
+++ src/test/java/com/vehiculerental/customers/CustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehiculerental.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
